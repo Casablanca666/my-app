61bab7a9156a2c71841be9a0d6f4fe942ba7a5f1
zzh <2528307487@qq.com>
2024-04-16 15:10:01 +0800

    my-app

---
+++ my-app/src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import User from '../views/User.vue'
+import Main from '../views/Main.vue'
+import Mall from '../views/Mall.vue'
+import PageOne from '../views/PageOne.vue'
+import PageTwo from '../views/PageTwo.vue'
+
+Vue.use(VueRouter)  
+//1.创建路由组件
+//2.将路由与组件进行映射
+//3.创建router实例
+//4.创建和挂载根实例
+//5.路由出口
+const  routes = [
+    {path:'/',
+    component:Main,
+    redirect:'/home',
+    children:[
+        //子路由
+        {path:'home',component:Home},
+        {path:'user',component:User},
+        {path:'mall',component:Mall},
+        {path:'page1',component:PageOne},
+        {path:'page2',component:PageTwo}     
+    ]
+},
+   
+]
+
+const router = new VueRouter({
+     routes   //（缩写） 相当于routes:routes
+})
+
+export default router
+
---
+++ my-app/src/views/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside width="auto"> <commonAside /> </el-aside>
+      <el-container>
+        <el-header><commonHeader /></el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script>
+import commonAside from "../components/CommonAside.vue";
+import commonHeader from "../components/CommonHeader.vue";
+export default {
+  components: {
+    commonAside,
+    commonHeader,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.el-header {
+  padding: 0;
+}
+</style>
